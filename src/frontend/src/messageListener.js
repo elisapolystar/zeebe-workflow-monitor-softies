cf9fca4322c9ccb220ea18108ed88fde6b7b2f31
Ilmari Luojus <ilmari.luojus@tuni.fi>
2023-10-07 18:09:24 +0300

    Recieve and display messages from backend

---
+++ src/frontend/src/messageListener.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import MessageDisplayer from './messageDisplayer'; 
+
+const MessageListener = () => {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        // Update the URL once known
+        const socket = io('http://server:8000');
+
+        // Define event handlers for messages received from the server.
+        socket.on('message', (message) => {
+            // Handle incoming messages here and update your component state.
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        // Clean up the WebSocket connection when the component unmounts.
+        return () => {
+            socket.disconnect();
+        };
+    }, []);
+
+    return (
+        <div>
+            <MessageDisplayer messages={messages} /> 
+        </div>
+    );
+};
+
+export default MessageListener;
