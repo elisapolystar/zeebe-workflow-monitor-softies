4dd3c8f5df9e4ce451e740de31f075fc27215f40
Ilmari Luojus <ilmari.luojus@tuni.fi>
2023-11-30 19:57:27 +0200

    merge conflict solved v2

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
 import Header from './components/Header.tsx'
 import App from './App.tsx';
-=======
-import MessageListener from './messageListener';
-import NavBar from './components/NavBar';
-import Header from './components/Header'
->>>>>>> main
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -17,8 +11,4 @@ root.render(
       <App />
     </div>
   </React.StrictMode>,
-<<<<<<< HEAD
 ); export {root};
-=======
-);
->>>>>>> main
