00645270c670711f6fb5929e96ec41f5c6b7dfb5
Kalle Karttunen <kalle.a.karttunen@gmail.com>
2023-12-11 21:28:21 +0200

    removed redundant db connection

--- src/backend/consumer/DatabaseHandler.go
+++ src/backend/consumer/DatabaseHandler.go
@@ -295,7 +295,6 @@ func RetrieveProcessByID(db *sql.DB, key int64) string {
 func RetrieveInstanceByID(db *sql.DB, column string, key int64) (string, error) {
 	if (column == "ProcessDefinitionKey") || (column == "ProcessInstanceKey") {
 		fmt.Println("retrieving an instance")
-		db, err := connectToDatabase()
 		if err != nil {
 			fmt.Println("Error opening database connection")
 		}
