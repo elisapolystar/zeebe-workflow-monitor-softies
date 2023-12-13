bf2659a5fd6df73d0d3be943ddaed50d410f2d57
jalmarimieho <jalmari.mieho@tuni.fi>
2023-12-13 23:01:26 +0200

    comments to the consumer

--- src/backend/consumer/Consumer.go
+++ src/backend/consumer/Consumer.go
@@ -9,6 +9,7 @@ import (
 	"github.com/IBM/sarama"
 )
 
+// Subscribe to wanted kafka topics, listen to messages from these topics and pass the messages with their corresponding topics to a channel
 func Consume(messageChannel chan<- topicMessagePair) {
 
 	// Define the Kafka broker address and topic we want to subscribe to
@@ -35,7 +36,7 @@ func Consume(messageChannel chan<- topicMessagePair) {
 	config := sarama.NewConfig()
 	config.Consumer.Return.Errors = true
 
-	// Create a Kafka consumer
+	// Create a Kafka consumer that will be used to create partition consumers for each topic
 	consumer, err := sarama.NewConsumer(brokers, config)
 	if err != nil {
 		fmt.Printf("Error creating Kafka consumer: %v\n", err)
@@ -52,11 +53,13 @@ func Consume(messageChannel chan<- topicMessagePair) {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt)
 
-	// Create a map to store partition consumers for each topic
+	// Create a map to store partition consumers for each topic, topic is the key and the partition consumer is the value
 	partitionConsumers := make(map[string]sarama.PartitionConsumer)
 
-	// Subscribe to each topic
+	// Subscribe to each topic declared in the topics list
 	for _, topic := range topics {
+
+		// Create a partition consumer for a specific topic
 		partitionConsumer, err := consumer.ConsumePartition(topic, 0, sarama.OffsetOldest)
 		if err != nil {
 			fmt.Printf("Error subscribing to topic %s: %v\n", topic, err)
@@ -68,31 +71,37 @@ func Consume(messageChannel chan<- topicMessagePair) {
 			}
 		}(topic)
 
+		// Add the partition consumer of a specific topic to the map with the corresponding topic as the key
 		partitionConsumers[topic] = partitionConsumer
 		fmt.Printf("Subscribed to topic: %s\n", topic)
 	}
 
-	// Consume messages from the Kafka topics
+	// Infinite loop that listens to all the topics in the partitionConsumers map
+	// If we get a message, the message and the topic are passed to the channel
 	for {
 		select {
+		// If there is a termination signal
 		case <-signals:
 			fmt.Println("Received termination signal. Closing consumer.")
 			return
 		default:
+			// Loop the consumers of the topics if they have messages
 			for topic, partitionConsumer := range partitionConsumers {
 				select {
+				// Receive a message from some of the partition consumers
 				case msg := <-partitionConsumer.Messages():
 
+					// Put the received message and the topic to the channel
 					messageChannel <- topicMessagePair{topic, msg.Value}
-
 				case err := <-partitionConsumer.Errors():
 					fmt.Printf("Error consuming message from topic %s: %v\n", topic, err)
+
+				// If there are no messages or errors from this topic we continue to the next item in the for-loop
 				default:
 					continue
 				}
 			}
 		}
-
 		// Pause for 0.1 seconds to save cpu usage
 		time.Sleep(100 * time.Millisecond)
 	}
